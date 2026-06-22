db4662847c91a7339014494cc8a55d8eacf09a6b
jpvassoler <joaovassoler18@icloud.com>
2026-06-22 19:42:52 -0300

    script

--- script.js
+++ script.js
@@ -29,3 +29,22 @@ elements.forEach((element) => {
     myObserver.observe(element);
 
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function(e){
+
+        const destino = document.querySelector(this.getAttribute("href"));
+
+        if(destino){
+
+            e.preventDefault();
+
+            destino.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+
+        }
+
+    });
+});
